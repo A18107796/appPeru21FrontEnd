e2ea0d457a637d66efd0692f8163920edff56868
Ricardo <A18107796@idat.edu.pe>
2021-06-17 22:00:55 -0500

    Avance Cerro

--- src/app/pages/gestionAcademica/especializaciones/edit-especializaciones/modal-cursos-especializaciones/modal-cursos-especializaciones.component.ts
+++ src/app/pages/gestionAcademica/especializaciones/edit-especializaciones/modal-cursos-especializaciones/modal-cursos-especializaciones.component.ts
@@ -12,6 +12,9 @@ export class ModalCursosEspecializacionesComponent implements OnInit {
   @Output('cursoSeleccionado') cursoSeleccionado = new EventEmitter();
   constructor(public modalService: ModalService, private sC: CursoService) { }
   cursos: Curso[] = [];
+  key: Map<number, Curso> = new Map();
+  
+
   ngOnInit(): void {
     this.sC.emitCursos.subscribe(listaCursos => {
       console.log(listaCursos);
@@ -22,6 +25,30 @@ export class ModalCursosEspecializacionesComponent implements OnInit {
 
   enviarCurso(curso: Curso) {
     this.cursoSeleccionado.emit(curso);
+  }
+
+  enviarLista() {
+    console.log(this.key);
+
+  }
+
+  cerrarModal() {
+    this.modalService.cerrarModal();
+    this.key = new Map();
+  }
+
+  setOption(obj: Curso, evt: any) {
+    let id = evt.target.id;
+    if (evt.target.checked) {
+      this.key.set(id, obj);
+
+    } else {
+      this.key.delete(id)
+    }
+
+
+
+
   }
 
 
